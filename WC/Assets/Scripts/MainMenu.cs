7610e7f2dd68c4eee09ce28439c26b60365f3eaa
Colton <Colton.Spruill@gmail.com>
2025-04-22 06:30:05 -0500

    How to play panel fixed.
    Can now swap how to plan panels on button clicks.

--- WC/Assets/Scripts/MainMenu.cs
+++ WC/Assets/Scripts/MainMenu.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject[] howToPlayPanels;
+    [SerializeField] int howToPlayPanelIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void IncrementHowToPlayPanel()
+    {
+        howToPlayPanels[howToPlayPanelIndex].gameObject.SetActive(false);
+
+        howToPlayPanelIndex++;
+
+        if (howToPlayPanelIndex >= howToPlayPanels.Length)
+        {
+            howToPlayPanelIndex = 0;
+        }
+
+        howToPlayPanels[howToPlayPanelIndex].gameObject.SetActive(true);
+    }
+
+    public void DecrementHowToPlayPanel()
+    {
+        howToPlayPanels[howToPlayPanelIndex].gameObject.SetActive(false);
+        
+        howToPlayPanelIndex--;
+
+        if(howToPlayPanelIndex < 0)
+        {
+            howToPlayPanelIndex = howToPlayPanels.Length - 1;
+        }
+
+        howToPlayPanels[howToPlayPanelIndex].gameObject.SetActive(true);
     }
 }
