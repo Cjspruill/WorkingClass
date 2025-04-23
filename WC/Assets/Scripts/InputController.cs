a20051eb3c19fde8e55087de6d2a95aa047f1b40
Sniffzergn <42846731+Sniffzergn@users.noreply.github.com>
2025-04-22 22:13:19 -0500

    Input Commands
    Attached to players and not AI

--- WC/Assets/Scripts/InputController.cs
+++ WC/Assets/Scripts/InputController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
+
+    [SerializeField] PlayerControls playerControls;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class InputController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+    public void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    public void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
 }
