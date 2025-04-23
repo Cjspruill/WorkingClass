91951402261539fb36ad6d21366fc8763e0be261
Colton <Colton.Spruill@gmail.com>
2025-04-23 06:13:20 -0500

    More prefab updates. Combo system fixed for ai.

--- WC/Assets/Scripts/AIController.cs
+++ WC/Assets/Scripts/AIController.cs
@@ -16,22 +16,44 @@ public class AIController : MonoBehaviour
     //Current ai state
     [SerializeField] AIStates curState;
 
+    //Normal combo setup
+    [SerializeField] bool comboActive;
 
     [SerializeField] int comboIndex;
     [SerializeField] float comboTimer;
     [SerializeField] float comboTime;
-    [SerializeField] float comboTimeMin = .25f;
+    [SerializeField] float comboTimeMin = .5f;
     [SerializeField] float comboTimeMax = 3f;
 
-    [SerializeField] bool canAttack;
+    //Create a small timer buffer to slow down the attacks
+    [SerializeField] float idleBufferTimer;
+    [SerializeField] float idleBufferTime;
+    [SerializeField] float idleBufferTimeMin = .5f;
+    [SerializeField] float idleBufferTimeMax = 3f;
 
+    //Timer for idle to go back to attacking
+    [SerializeField] float idleTimer;
+    [SerializeField] float idleTime;
+    [SerializeField] float idleTimeMin = 3f;
+    [SerializeField] float idleTimeMax = 8f;
+
+
+    //Sprite renederer for visual color info
     [SerializeField] SpriteRenderer spriteRenderer;
+
+    [SerializeField] PlayerController playerReference;
+    [SerializeField] float distance;
+    [SerializeField] float speed;
     
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         comboTime = Random.Range(comboTimeMin, comboTimeMax);
+        idleBufferTime = Random.Range(idleBufferTimeMin, idleBufferTimeMax);
+        idleTime = Random.Range(idleTimeMin, idleTimeMax);
+
+        playerReference = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
@@ -47,33 +69,72 @@ public class AIController : MonoBehaviour
         switch (curState)
         {
             case AIStates.Idle:
+
+                if (comboActive)
+                {
+                    idleBufferTimer++;
+
+                    if (idleBufferTimer > idleBufferTime)
+                    {
+                        idleBufferTimer = 0;
+                        idleBufferTime = Random.Range(idleBufferTimeMin, idleBufferTimeMax);
+                        TryAttack();
+                    }
+                }
+                else
+                {
+                    idleTimer++;
+
+                    if(idleTimer > idleTime)
+                    {
+                        idleTimer = 0;
+                        //Chase first, then attack
+                        curState = AIStates.Chase;
+                    }
+                }
+
+
+
                 break;
             case AIStates.Chase:
+                distance = Vector2.Distance(transform.position, playerReference.transform.position);
+                Vector2 direction = playerReference.transform.position - transform.position;
+                direction.Normalize();
+
+                if (distance < 9 && distance >= 1.5)
+                {
+                    transform.position = Vector2.MoveTowards(this.transform.position, playerReference.transform.position, speed * Time.deltaTime);
+                }
+                else if (distance < 1.5f)
+                {
+                    TryAttack();
+
+                }
+
                 break;
             case AIStates.Attack:
 
-              comboTimer++;
-               
+                comboTimer++;
 
-
-                if(comboTimer >= comboTime && comboIndex <= 3)
+                if(comboTimer> comboTime)
                 {
-                    comboTimer = 0;
-                    comboIndex++;
-                    canAttack = true;
-                    spriteRenderer.color = Color.red;
-                    Invoke("CancelAttack", .1f);
-                    comboTime = Random.Range(comboTimeMin, comboTimeMax);
-
-               
-                        TryAttack();
+                    if(comboIndex <= 3)
+                    {
+                        comboIndex++;
+                        comboTimer = 0;
+                        comboTime = Random.Range(comboTimeMin,comboTimeMax);
+                        spriteRenderer.color = Color.red;
+                        Invoke("CancelAttack", .1f);
+                        comboActive = true;
+                        curState = AIStates.Idle;
+                    }
                     
+                    if (comboIndex > 3)
+                    {
+                        comboIndex = 0;
+                        comboActive = false;
+                    }
                 }
-                else if(comboIndex > 3)
-                {
-                    comboIndex = 0;
-                }
-
                 break;
         }
     }
@@ -81,7 +142,7 @@ public class AIController : MonoBehaviour
 
     public void TryAttack()
     {
-        int randVal = Random.Range(0,10000);
+        int randVal = Random.Range(0,500);
         
        if(randVal <= 1)
         {
@@ -91,7 +152,6 @@ public class AIController : MonoBehaviour
 
     public void CancelAttack()
     {
-        canAttack = false;
         spriteRenderer.color = Color.white;
     }
 }
