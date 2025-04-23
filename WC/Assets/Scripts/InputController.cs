98bf0c8cb81d796ac91247ebee63598dc15f95fd
Colton <Colton.Spruill@gmail.com>
2025-04-23 10:36:42 -0500

    Had to fight with the system on setting up the input.

--- WC/Assets/Scripts/InputController.cs
+++ WC/Assets/Scripts/InputController.cs
@@ -1,32 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputController : MonoBehaviour
 {
 
-    [SerializeField] PlayerControls playerControls;
+    [SerializeField] PlayerInput playerInput;
 
+    [SerializeField] InputAction moveAction;
+    [SerializeField] InputAction punchAction;
+    [SerializeField] InputAction kickAction;
+
+    //Moves the player
+    [SerializeField] PlayerController playerController;
+    //Attacks the opponent
+   // [SerializeField] 
+
+    public void OnEnable()
+    {
+        playerInput = new PlayerInput();
+
+        playerInput.Player.Enable();
+
+        playerInput.Player.Move.performed += SetMovement;
+    }
+
+    public void OnDisable()
+    {
+        playerInput.Player.Disable();
+    }
+
+    private void Awake()
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 
-    public void OnEnable()
+    public void SetMovement(InputAction.CallbackContext ctx)
     {
-        playerControls.Enable();
-    }
+        //Pass this to controller
+       Vector2 moveInput = ctx.ReadValue<Vector2>();
 
-    public void OnDisable()
-    {
-        playerControls.Disable();
     }
 
 }
--- WC/Assets/Scripts/PlayerController.cs
+++ WC/Assets/Scripts/PlayerController.cs
@@ -18,4 +18,9 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    public void MoveCharacter(Vector2 direction)
+    {
+
+    }
 }
