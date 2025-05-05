0d9262a7778e137c82f2192ee5131aff9d593345
Colton <Colton.Spruill@gmail.com>
2025-05-05 09:35:27 -0500

    Re adding tmpro

--- WC/Assets/Scripts/ComboController.cs
+++ WC/Assets/Scripts/ComboController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ComboController : MonoBehaviour
 {
+    [SerializeField] BoxCollider2D attackCollider;
+
     // Start is called before the first frame update
     void Start()
     {
